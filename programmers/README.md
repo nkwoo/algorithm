e42eb1bc7855c02402a80838382ba51733668885
NKWOO <nkw9986@gmail.com>
2021-07-30 01:59:25 +0900

    add 2021 kakao level 1

--- programmers/README.md
+++ programmers/README.md
@@ -3,3 +3,13 @@
 > 알고 있는 알고리즘 지식으로 코딩테스트를 풀어보았습니다.
 
 [프로그래머스](https://programmers.co.kr/)
+
+## 2021 KAKAO BLIND RECRUITMENT
+
+<img src="../resource/2021kakao_large2.png" width="1000" height="230" alt="2021 KAKAO BLIND RECRUITMENT"/>
+
+[프로그래머스 링크](https://programmers.co.kr/competitions/317/2021-kakao-blind-recruitment)
+
+|스탭별|바로가기|소스코드|
+|:---:|:---:|:---:|
+|문제 1 – 아이디 추천|https://programmers.co.kr/learn/courses/30/lessons/72410|[바로가기](./coding-test/2021-kakao-blind-recruitment/level1.java)|
---
+++ programmers/coding-test/2021-kakao-blind-recruitment/level1.java
@@ -0,0 +1,47 @@
+public class Solution {
+    public String solution(String new_id) {
+        return verifyUserId(new_id);
+    }
+
+    private String verifyUserId(String userId) {
+        userId = toLowerUserId(userId);
+
+        userId = replaceCharacter(userId,"[^a-z0-9-_.]", "");
+
+        userId = replaceCharacter(userId,"[.]+", ".");
+
+        userId = removeStartEndWithDot(userId);
+
+        userId = checkEmptyString(userId);
+
+        userId = substringBy16(userId);
+
+        userId = appendUserId(userId);
+
+        return userId;
+    }
+
+    private String toLowerUserId(String userId) {
+        return userId.toLowerCase();
+    }
+
+    private String replaceCharacter(String userId, String regex, String exchangeString) {
+        return userId.replaceAll(regex, exchangeString);
+    }
+
+    private String removeStartEndWithDot(String userId) {
+        return userId.startsWith(".") ? userId.substring(1) : userId.endsWith(".") ? userId.substring(0, userId.length() - 1) : userId;
+    }
+
+    private String checkEmptyString(String userId) {
+        return "".equals(userId) ? "a" : userId;
+    }
+
+    private String substringBy16(String userId) {
+        return removeStartEndWithDot(userId.length() > 15 ? userId.substring(0, 15) : userId);
+    }
+
+    private String appendUserId(String userId) {
+        return userId.length() < 3 ? appendUserId(userId + userId.substring(userId.length() - 1)) : userId;
+    }
+}
