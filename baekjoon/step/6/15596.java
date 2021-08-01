baf5224de0a4ecd618d7fc585290c09fa2e6debd
NKWOO <nkw9986@gmail.com>
2021-08-01 16:48:22 +0900

    add baekjoon 15596

---
+++ baekjoon/step/6/15596.java
@@ -0,0 +1,15 @@
+/**
+ * Title : 정수 N개의 합
+ * Link : https://www.acmicpc.net/problem/15596
+ */
+public class Test {
+    long sum(int[] a) {
+        long ans = 0;
+
+        for (int index : a) {
+            ans += index;
+        }
+
+        return ans;
+    }
+}
